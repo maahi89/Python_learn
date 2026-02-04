ccbeeb0a6c761c8b0731b27e202934b43a517270
maahi89 <mahithaguda2@gmail.com>
2026-02-04 15:40:24 +0530

    contact book

--- Coding_question/Password_generator.py
+++ Coding_question/Password_generator.py
@@ -1,9 +1,9 @@
-import random
-import string
-length=input("Enter the length of the password: ")
-password=" "
-characters = string.ascii_letters + string.digits + string.punctuation
-for i in range(int(length)):
-    password+= random.choice(characters)
-print("generated password is", password)    
-    
+while True:
+        import random
+        import string
+        length=input("Enter the length of the password: ")
+        password=" "
+        characters = string.ascii_letters + string.digits + string.punctuation
+        for i in range(int(length)):
+            password += random.choice(characters)
+        print("generated password is", password)    
