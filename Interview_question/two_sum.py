eab7c4506d1e3e7d502933af2db1c3d35e9ef29a
maahi89 <mahithaguda2@gmail.com>
2026-02-19 12:50:01 +0530

    using post method added successfully

--- Interview_question/two_sum.py
+++
@@ -1,8 +0,0 @@
-nums=[1,2,3,4,5,6,7,8,9,4,5,6]
-target=9
-seen={}                         #seen is to check wether its in the list
-for i,num in enumerate(nums):   #enumerate to give index and value
-    diff=target-num
-    if diff in seen:
-        print(seen[diff], i)
-    seen[num]=i    
