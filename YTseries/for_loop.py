23a1c6054846518c32d178c711ed4e55e59d3344
maahi89 <mahithaguda2@gmail.com>
2026-02-25 16:14:26 +0530

    new notes

---
+++ YTseries/for_loop.py
@@ -0,0 +1,25 @@
+name="mahitha"
+for i in name:
+    print(i)
+print()
+
+
+for i in range(1,11):
+    print(i)
+print()
+
+
+for i in range(1,10,2):
+    print(i)
+print()
+
+
+for i in range(10,0,-1):
+    print(i)
+
+
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+    else:
+        print("odd number")
