05c7effe67d3019a81f1af506850d89cb0878f75
maahi89 <mahithaguda2@gmail.com>
2026-02-26 00:12:25 +0530

    while loop

---
+++ YTseries/while_loop.py
@@ -0,0 +1,28 @@
+i = 1
+
+while i <= 5:
+    print("mahitha")
+    i += 1
+
+
+num = 1 
+while num <= 10:
+    print(num)
+    num += 1
+
+num=2
+while num <= 10:
+    print(num)
+    num += 2
+
+
+num=10
+while num > 0:
+    print(num)
+    num -= 1    
+
+text = ""
+
+while text != "stop":
+    text = input("Enter something (type stop to exit): ")
+    print("You entered:", text)
