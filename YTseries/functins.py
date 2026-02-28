6b2277ab40d46d7af1a9137ebfbf1e50159d3ca9
maahi89 <mahithaguda2@gmail.com>
2026-02-28 23:52:07 +0530

    functions

--- YTseries/functins.py
+++ YTseries/functins.py
@@ -5,3 +5,10 @@ def add(a, b):
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 print(add(a, b))
+
+def add_numbers(*numbers):
+    return sum(numbers)
+
+result = add_numbers(1, 2, 3, 4)
+print(result)
+# print(add_numbers(1,2,3,4))
