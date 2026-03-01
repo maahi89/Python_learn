9a3ab647d9b97c2c9111839b71f41bc7abd54d5f
maahi89 <mahithaguda2@gmail.com>
2026-03-01 23:14:34 +0530

    aruments in python

--- YTseries/args.py
+++ YTseries/args.py
@@ -1,3 +1,24 @@
 def add(*args):
     return sum(args)
 print(add(1, 2, 3, 4))
+
+
+def add(*numbers):
+    total = 5
+    for num in numbers:
+        total = total + num
+    return total
+print(add(1, 2, 3, 4))
+
+
+def message(*messages):
+    for msg in messages:
+        print(msg)
+message("Hello", "Lets do it fast!")
+
+
+def example(a, *args):
+    print("a:", a)
+    print("args:", args)
+
+example(7, 2, 3, 4)
