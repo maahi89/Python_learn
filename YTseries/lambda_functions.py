fa4a14e2e94160f9e434d1e80db4e9e24407b6e7
maahi89 <mahithaguda2@gmail.com>
2026-03-04 00:19:25 +0530

    lambda functions

--- YTseries/lambda_functions.py
+++ YTseries/lambda_functions.py
@@ -1,9 +1,21 @@
-students = [
-    ("Mahitha", 85),
-    ("Rahul", 90),
-    ("Anu", 75)
-]
+# students = [
+#     ("Mahitha", 85),
+#     ("Rahul", 90),
+#     ("Anu", 75)
+# ]
 
-students.sort(key=lambda x: x[1])
-print(students)
+# students.sort(key=lambda x: x[1])
+# print(students)
+
+
+# add = lambda a, b: a + b
+# print(add(5, 3))
+
+# square = lambda x: x*x
+# print(square(3))
+
+
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x*x, numbers))
+print(squared)
 
