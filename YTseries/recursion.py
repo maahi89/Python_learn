88dc3a8f09d0f9638e26d3e4c2e4adcf572c4ce9
maahi89 <mahithaguda2@gmail.com>
2026-03-07 00:24:49 +0530

    Fibonacci series in python

--- YTseries/recursion.py
+++ YTseries/recursion.py
@@ -6,8 +6,15 @@
 # print(count(7))
 
 
-def factorial(n):
-    if n == 0:
-        return 1
-    return n * factorial(n-1)
-print(factorial(5))
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     return n * factorial(n-1)
+# print(factorial(5))
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(10))
