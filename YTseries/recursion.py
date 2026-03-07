2f2418d1a47a257d67cf823ab45bbaa17575df34
maahi89 <mahithaguda2@gmail.com>
2026-03-07 23:56:03 +0530

    special functions

--- YTseries/recursion.py
+++ YTseries/recursion.py
@@ -1,16 +1,16 @@
-# def count(n):
-#     if n == 0:
-#         return 
-#     print(n)
-#     count(n-1)
-# print(count(7))
+def count(n):
+    if n == 0:
+        return 
+    print(n)
+    count(n-1)
+print(count(7))
 
 
-# def factorial(n):
-#     if n == 0:
-#         return 1
-#     return n * factorial(n-1)
-# print(factorial(5))
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+print(factorial(5))
 
 
 def fibonacci(n):
