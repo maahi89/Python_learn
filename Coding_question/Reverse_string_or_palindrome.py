1cd6a9227614dc5ddd4933405fc28ee3e9b033ad
maahi89 <mahithaguda2@gmail.com>
2026-03-21 19:16:09 +0530

    Play lengthd

--- Coding_question/Reverse_string_or_palindrome.py
+++ Coding_question/Reverse_string_or_palindrome.py
@@ -6,3 +6,14 @@ rev=""
 for i in name:
     rev=i+rev
 print(rev)    
+
+name = "madam"
+if name == name[::-1]:
+    print(f"{name} is a palindrome")
+else:    
+    print(f"{name} is not a palindrome")
+
+name = "Rahul"
+print(name[::-1])
+print(name[0:2])
+print(name[-1:-3:-1])
