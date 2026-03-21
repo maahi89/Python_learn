b1d1747f15003b7e44d926e4042034478101b2c2
maahi89 <mahithaguda2@gmail.com>
2026-03-21 18:42:43 +0530

    new series

--- Coding_question/fibonacci_series_using_generators.py
+++ Coding_question/fibonacci_series_using_generators.py
@@ -1,10 +1,17 @@
-def fibonacci(n):    #Formula (Conceptual)F(n) = F(n-1) + F(n-2)
+# def fibonacci(n):    #Formula (Conceptual)F(n) = F(n-1) + F(n-2)
+#     a,b=0,1
+#     for i in range(n):
+#         yield a
+#         a,b=b,a+b
+# for num in fibonacci (50):
+#     print(num , end=" ") 
+
+# print(num)    
+
+def fib(n):
     a,b=0,1
     for i in range(n):
+        a, b= b, a+b
         yield a
-        a,b=b,a+b
-for num in fibonacci (50):
-    print(num) 
-
-print(num)    
-print(num)
+for num in fib(30):
+    print(num)
