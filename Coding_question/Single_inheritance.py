d18e15ba856ec25cdd4b075be7a74079e21b55aa
maahi89 <mahithaguda2@gmail.com>
2026-03-23 17:21:46 +0530

    Single inheritance

---
+++ Coding_question/Single_inheritance.py
@@ -0,0 +1,15 @@
+class person():
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+class student(person):
+    def __init__(self, name, age, rollno):
+        super().__init__(name, age)
+        self.rollno=rollno
+s1 = student(*input("Enter name, age and rollno of student: ").split())
+print("Name: ", s1.name)
+print("Age: ", s1.age)          
+print("Rollno: ", s1.rollno)    
+
+
+
