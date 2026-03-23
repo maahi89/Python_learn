40b2b78ffeae9dc0913e20e33d5377ac59b61b53
maahi89 <mahithaguda2@gmail.com>
2026-03-23 17:26:56 +0530

    pushing

--- Coding_question/Single_inheritance.py
+++ Coding_question/Single_inheritance.py
@@ -9,4 +9,5 @@ class student(person):
 s1 = student(*input("Enter name, age and rollno of student: ").split())
 print("Name: ", s1.name)
 print("Age: ", s1.age)          
-print("Rollno: ", s1.rollno)    
+print("Rollno: ", s1.rollno)
+    
