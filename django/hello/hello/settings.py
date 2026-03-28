a173bbd309ccac29bf99f1b4a8708e5bca65d797
maahi89 <mahithaguda2@gmail.com>
2026-03-29 00:28:06 +0530

    details to show through templates

--- django/hello/hello/settings.py
+++ django/hello/hello/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home', # this is our app that we created
 ]
 
 MIDDLEWARE = [
