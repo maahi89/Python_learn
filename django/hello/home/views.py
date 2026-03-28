6856220dca0cc2bbb634b40e47e01b9e69e61c05
maahi89 <mahithaguda2@gmail.com>
2026-03-29 00:26:32 +0530

    passing values to templates

--- django/hello/home/views.py
+++ django/hello/home/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, HttpResponse
+from .models import Student
 
 def index(request):
-    # return HttpResponse("You're at the home index.")
-    return render(request, 'index.html') # this will look for index.html file in templates folder and render it
+    data = {
+        "name": "Mahitha",
+        "age": 20,
+        "hobbies": ["coding", "reading", "traveling"],
+        "fav_color": ["blue", "green", "red"],
+        "students": Student.objects.all() # this will fetch all the students from the database and pass it to the template
+    }
+    return render(request, 'index.html', data) # this will look for index.html file in templates folder and render it
+
 
 def about(request):
-    return HttpResponse("You're at the about page.")
+    data = {
+        "about_me" : "i am a software developer and i love to learn new technologies and i am passionate about coding and i want to become a full stack developer in future"
+    }
+    return render(request, 'about.html', data)
+    #  return HttpResponse("You're at the services page.")
+
 
 def services(request):
     return HttpResponse("You're at the services page.")
 
 def contact(request):
-    return HttpResponse("You're at the contact page.")
+    data = {
+        "number": 1234567890,
+        "name": "Mahitha",
+        "mail": "mahitha@example.com"
+    }
+    return render(request, 'contact.html', data)
 
 
