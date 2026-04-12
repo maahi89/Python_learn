91d7ea58f5236ded96d16fcb2e0e75627c1d6dfd
maahi89 <mahithaguda2@gmail.com>
2026-04-12 20:45:28 +0530

    ecommerce with html and css

--- django/ecommerce/ecommerce/urls.py
+++ django/ecommerce/ecommerce/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.urls import path, include
 
 
 
@@ -31,5 +32,7 @@ urlpatterns = [
     path('', home), 
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
+    path('', include('store.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
